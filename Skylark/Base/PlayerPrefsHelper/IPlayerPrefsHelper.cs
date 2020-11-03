8a1e29b60b12326634dc51d0eccd2b6593654c0b
muyao <420418819@qq.com>
2020-11-03 17:57:29 +0800

    Add PlayerPrefs

---
+++ Skylark/Base/PlayerPrefsHelper/IPlayerPrefsHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Skylark
+{
+    public interface IPlayerPrefsHelper
+    {
+        void Init();
+        void Reset();
+        void Save();
+        bool GetBool(string key, bool defaultValue = false);
+
+        string GetString(string key, string defaultValue = "");
+
+        float GetFloat(string key, float defaultValue = 0);
+
+        int GetInt(string key, int defaultValue = 0);
+
+        void SetString(string key, string value);
+
+        void SetBool(string key, bool value);
+
+        void SetFloat(string key, float value);
+
+        void SetInt(string key, int value);
+    }
+
+    public class PrefsRecord
+    {
+        private static IPlayerPrefsHelper s_Record;
+        public static IPlayerPrefsHelper S
+        {
+            get
+            {
+                if (s_Record == null)
+                {
+                    s_Record = new PlayerPrefsHelper();
+                    s_Record.Init();
+                }
+                return s_Record;
+            }
+        }
+    }
+}
---
+++ Skylark/Base/PlayerPrefsHelper/PlayerPrefsHelper.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Skylark
+{
+    public class PlayerPrefsHelper : IPlayerPrefsHelper
+    {
+        public void Init()
+        {
+
+        }
+
+        public void Reset()
+        {
+        }
+
+        public void Save()
+        {
+        }
+
+        public void SetBool(string key, bool value)
+        {
+            float fvalue = -1;
+            if (value)
+            {
+                fvalue = 1;
+            }
+            else
+            {
+                fvalue = 0;
+            }
+            PlayerPrefs.SetFloat(key, fvalue);
+        }
+
+        public bool GetBool(string key, bool defaultValue = false)
+        {
+            float fvalue = PlayerPrefs.GetFloat(key, 0);
+
+            if (fvalue == 1 || defaultValue)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void SetFloat(string key, float value)
+        {
+            PlayerPrefs.SetFloat(key, value);
+        }
+
+        public float GetFloat(string key, float defaultValue = 0)
+        {
+            return PlayerPrefs.GetFloat(key, defaultValue);
+        }
+
+        public void SetInt(string key, int value)
+        {
+            PlayerPrefs.SetString(key, value.ToString());
+        }
+
+        public int GetInt(string key, int defaultValue = 0)
+        {
+            string svalue = PlayerPrefs.GetString(key);
+            int value;
+            if (!int.TryParse(svalue, out value))
+            {
+                return defaultValue;
+            }
+            return value;
+        }
+
+        public string GetString(string key, string defaultValue = "")
+        {
+            return PlayerPrefs.GetString(key, defaultValue);
+        }
+
+        public void SetString(string key, string value)
+        {
+            PlayerPrefs.SetString(key, value);
+        }
+    }
+}
