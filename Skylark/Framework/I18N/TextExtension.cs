7158e47e02f8218a403ed64d0106c662d9ecb93e
muyao <420418819@qq.com>
2020-11-06 17:28:43 +0800

    Add AutoTranslation Extension

---
+++ Skylark/Framework/I18N/TextExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Skylark
+{
+    public static class TextExtension
+    {
+        public static T SetTranText<T>(this T text, string key, params object[] m_Values) where T : Text
+        {
+            text.gameObject.AddComponent<AutoTranslation>().SetKeyValue(key, m_Values);
+            return text;
+        }
+    }
+}
